0542a3b22cd4dd1456ff1c53acd52efe98ff18b4
Tyvian10 <binanaapuassa@outlook.com>
2025-08-16 20:20:38 +0200

    Installer l'authentification (URGENT),Ajouter le système de rôles,Créer les controllers manquants

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             Nieuws Overzicht
         </h2>
     </x-slot>
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <a href="{{ route('news.create') }}" class="btn btn-primary mb-4">Nieuw Bericht</a>
             @foreach ($news as $item)
-                <div class="mb-4 p-4 bg-white dark:bg-gray-700 rounded shadow">
+                <div class="mb-4 p-4 bg-white rounded shadow">
                     <h2 class="text-lg font-bold">{{ $item->titel }}</h2>
                     <p>{{ $item->nieuwsbericht }}</p>
                     <a href="{{ route('news.show', $item->id) }}">Bekijk</a> |
